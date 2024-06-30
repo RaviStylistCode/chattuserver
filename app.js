9401bbc7698101dfc2c145da117e4725491ee8f5
Ravi Bhushan <ravibhu730@gmail.com>
2024-06-30 15:58:05 +0530

    fixed cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ import messageRouter from "./routes/messageRoute.js";
 const app = express();
 // app.use(cors());
 const option = {
-  origin: "http://localhost:5173",
+  origin: "*",
   credentials: true,
 };
 app.use(cors(option));
