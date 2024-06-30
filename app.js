d424a22a46b7f6e77baad440584a7fe4c8af8999
Ravi Bhushan <ravibhu730@gmail.com>
2024-06-30 18:04:39 +0530

    fixing er

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ import messageRouter from "./routes/messageRoute.js";
 
 
 const app = express();
-// app.use(cors());
+
 const option = {
-  origin: "*",
+  origin: [process.env.chattuclient,'http://localhost:5173'],
   credentials: true,
 };
 app.use(cors(option));
